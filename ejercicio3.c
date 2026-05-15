5658e34a8fae4f67c467892c04c9221880809a2a
Nicolas-Tituana <nicolas.tituana@udla.edu.ec>
2026-05-15 15:29:45 -0500

    terver commit

--- ejercicio3.c
+++ ejercicio3.c
@@ -3,8 +3,9 @@
 int main (int argc, char *argv[]){
     char vocales[6]={'a','e','i','o','u'};
     char *ptr;
-    ptr = vocales;
-    for ( int i = 0; i < 6; i++);
+    ptr = &vocales[3]; *ptr = 'x';
+    int i;
+    for (  i = 0; i < 5; i++);
     {
         printf("%c", *(ptr+i));
 
