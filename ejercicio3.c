663ee5226ca8659f45535d45e15db12793e53117
Nicolas-Tituana <nicolas.tituana@udla.edu.ec>
2026-05-15 15:23:36 -0500

    primer commit ejercicio 2

---
+++ ejercicio3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    char vocales[6]={'a','e','i','o','u'};
+    for ( int i = 0; i < 6; i++);
+    {
+        printf("%c\t", vocales [i]);
+
+    }
+    
+
+}
+    
