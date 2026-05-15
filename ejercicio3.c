69f7329bf83acbed4607b1a220164a4acc6b99bf
Nicolas-Tituana <nicolas.tituana@udla.edu.ec>
2026-05-15 15:26:05 -0500

    segundo commit

--- ejercicio3.c
+++ ejercicio3.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
-int main(){
+int main (int argc, char *argv[]){
     char vocales[6]={'a','e','i','o','u'};
+    char *ptr;
+    ptr = vocales;
     for ( int i = 0; i < 6; i++);
     {
-        printf("%c\t", vocales [i]);
+        printf("%c", *(ptr+i));
 
     }
+    return 0;
     
 
 }
